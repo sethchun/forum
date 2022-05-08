1b26b6704de00e6ef8400377b6f9fb693c491ae3
Seth Chun <schun.code@gmail.com>
2022-05-08 12:08:09 -0500

    rudimentary success

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-// your code here
+var button = document.getElementsByClassName('btn')[0];
+
+button.addEventListener('click', function () {
+    var message = document.getElementById('message').value;
+    var name = document.getElementById('name').value;
+    var newPost = document.createElement('div');
+    newPost.className = 'forum-post';
+    var newForumPostMessage = document.createTextNode(message);
+    var newForumPostName = document.createTextNode(name);
+    newPost.appendChild(newForumPostMessage);
+    newPost.appendChild(newForumPostName);
+    document.getElementsByClassName('posts')[0].append(newPost);
+});
